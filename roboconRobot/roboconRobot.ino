ec3e0c15d20abadf52d574c195cb7ac587c49ded
bkatona <30813752+bkatona@users.noreply.github.com>
2022-03-23 12:34:35 +0800

    Make debug flag
    Add debug flag to turn on and off printing to SerialUSB

--- roboconRobot/roboconRobot.ino
+++ roboconRobot/roboconRobot.ino
@@ -1,14 +1,16 @@
 //Firmware for Lingnan University Robocon robot wireless controller
-//Receives control commands to robot over Xbee using the Serial Transfer library 
+//Receives control commands to robot over Xbee using the Serial Transfer library
 //Calculates motor speeds and sends to Sabertooth motor controller
 
-//As of 23 March 2022 (Redboard Turbo board definition vs 1.8.6), 
+//As of 23 March 2022 (Redboard Turbo board definition vs 1.8.6),
 //must update board definitions for Sparkfun Redboard Turbo, see:
 //https://github.com/sparkfun/Arduino_Boards/pull/96
- 
+
 #include <SerialTransfer.h>
 #include <Sabertooth.h>
 #include "wiring_private.h" // pinPeripheral() function D2-TX, D3-RX
+
+const bool DEBUG = true;
 
 const int CTRL_MAX = 127;
 struct CTRL {
@@ -29,41 +31,41 @@ SerialTransfer ctrlTransfer;
 //}
 
 void setup() {
-//XBEE.begin(9600); //XBee
-SerialUSB.begin(9600);
-Serial1.begin(9600);//
-//ctrlTransfer.begin(XBEE);
-ctrlTransfer.begin(Serial1);
+  //XBEE.begin(9600); //XBee
+  SerialUSB.begin(9600);
+  Serial1.begin(9600);//
+  //ctrlTransfer.begin(XBEE);
+  ctrlTransfer.begin(Serial1);
 }
 
 void loop() {
 
-  if(ctrlTransfer.available()) {
+  if (ctrlTransfer.available()) {
     int recSize = 0;
     recSize = ctrlTransfer.rxObj(ctrlData, recSize);
-    
+
     int denominator = max(abs(ctrlData.driveSpeed) + abs(ctrlData.strafeSpeed) + abs(ctrlData.turnSpeed), CTRL_MAX);
-    int frontLeft = (ctrlData.driveSpeed + ctrlData.strafeSpeed + ctrlData.turnSpeed)* CTRL_MAX / denominator;
-    int backLeft = (ctrlData.driveSpeed - ctrlData.strafeSpeed + ctrlData.turnSpeed)* CTRL_MAX / denominator;
+    int frontLeft = (ctrlData.driveSpeed + ctrlData.strafeSpeed + ctrlData.turnSpeed) * CTRL_MAX / denominator;
+    int backLeft = (ctrlData.driveSpeed - ctrlData.strafeSpeed + ctrlData.turnSpeed) * CTRL_MAX / denominator;
     int frontRight = (ctrlData.driveSpeed - ctrlData.strafeSpeed - ctrlData.turnSpeed) * CTRL_MAX / denominator;
     int backRight = (ctrlData.driveSpeed + ctrlData.strafeSpeed - ctrlData.turnSpeed) * CTRL_MAX / denominator;
-    
-    char dat1[32];
-    sprintf(dat1, "Drive:%i,Strafe:%i,Turn:%i\r\n",ctrlData.driveSpeed,ctrlData.strafeSpeed,ctrlData.turnSpeed);
-    SerialUSB.print(dat1);
 
-    SerialUSB.print(frontLeft);
-    SerialUSB.print("    ");
-    SerialUSB.println(frontRight);
-    SerialUSB.println("    ");
-    SerialUSB.println("    ");
-    SerialUSB.print(backLeft);
-    SerialUSB.print("    ");
-    SerialUSB.println(backRight);
-    
+    if (DEBUG == true) {
+      char dat1[32];
+      sprintf(dat1, "Drive:%i,Strafe:%i,Turn:%i\r\n", ctrlData.driveSpeed, ctrlData.strafeSpeed, ctrlData.turnSpeed);
+      SerialUSB.print(dat1);
+      SerialUSB.print(frontLeft);
+      SerialUSB.print("    ");
+      SerialUSB.println(frontRight);
+      SerialUSB.println("    ");
+      SerialUSB.println("    ");
+      SerialUSB.print(backLeft);
+      SerialUSB.print("    ");
+      SerialUSB.println(backRight);
+    }
+
   }
-  else if(ctrlTransfer.status<0) {
+  else if (ctrlTransfer.status < 0) {
     SerialUSB.print("Error");
   }
-
 }
